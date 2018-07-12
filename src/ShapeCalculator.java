a1779e48deb3d1843d3319c285b999b7d0f6d283
Agnieszka Łozowska <aga.lozowska@wp.pl>
2018-07-12 16:37:14 +0200

    poprawiony kod

--- src/ShapeCalculator.java
+++ src/ShapeCalculator.java
@@ -1,23 +1,18 @@
 public class ShapeCalculator {
 
-    double squareArea;
-    double circleArea;
-    double trianglePerimeter;
-    double rectPerimeter;
-
     double squareArea(Square square) {
-        return squareArea= square.a * square.a;
+        return square.a * square.a;
     }
 
     double circleArea(Circle circle) {
-        return circle.circleArea = 3.14 * circle.r * circle.r;
+        return 3.14 * circle.r * circle.r;
     }
 
     double trianglePerimeter(Triangle triangle) {
-        return triangle.trianglePerimeter = triangle.a + triangle.b + triangle.c;
+        return triangle.a + triangle.b + triangle.c;
     }
 
     double rectPerimeter(Rectangle rectangle) {
-        return rectangle.rectPerimeter = 2 * rectangle.a + 2 * rectangle.b;
+        return 2 * rectangle.a + 2 * rectangle.b;
     }
 }
--- src/TestShapeCalculator.java
+++ src/TestShapeCalculator.java
@@ -2,12 +2,16 @@ public class TestShapeCalculator {
     public static void main(String[] args) {
         ShapeCalculator shapeCalculator=new ShapeCalculator();
         Square square = new Square(5);
+        double poleKwartatu=shapeCalculator.squareArea(square);
         Rectangle rectangle = new Rectangle(2.0, 4.2);
+        double obwodProstokata=shapeCalculator.rectPerimeter(rectangle);
         Circle circle = new Circle(4.9);
+        double poleKola=shapeCalculator.circleArea(circle);
         Triangle triangle=new Triangle(5.5,5.5, 5.5);
+        double obwodTrojkata = shapeCalculator.trianglePerimeter(triangle);
 
 
 
-        System.out.println(square.squareArea +" \n"+rectangle.rectPerimeter+ " \n"+triangle.trianglePerimeter+" \n"+rectangle.rectPerimeter);
+        System.out.println(poleKwartatu +" \n"+poleKola+ " \n"+obwodProstokata+" \n"+obwodTrojkata);
     }
 }
